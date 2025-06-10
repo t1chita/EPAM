77de36530e5e1946b108f8ded8a0af0833a07048
t1chit <temur.chitashvili@space.ge>
2025-06-10 12:10:00 +0400

    Initial commit for calculator app

---
+++ UIKit/App Lifecycle and main components/Calculator App/Calculator App/Source/CalculatorPage/CalculatorVC.swift
@@ -0,0 +1,16 @@
+//
+//  CalculatorVC.swift
+//  Calculator App
+//
+//  Created by Temur Chitashvili on 10.06.25.
+//
+
+import UIKit
+
+class CalculatorVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
