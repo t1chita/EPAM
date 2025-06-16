2d94e00aeb65227b8dd3852615b5cf328320635e
t1chit <temur.chitashvili@space.ge>
2025-06-16 20:32:31 +0400

    Add SettingsViewController

--- UIKit/Develop multi-tab app/Develop multi-tab app/Tabs/SettingsViewController.swift
+++ UIKit/Develop multi-tab app/Develop multi-tab app/Tabs/SettingsViewController.swift
@@ -8,10 +8,45 @@
 import UIKit
 
 final class SettingsViewController: UIViewController {
+    private lazy var toggleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Navigation is easy!"
+        label.font = .systemFont(ofSize: 18, weight: .medium)
+        label.textColor = .white
+        return label
+    }()
+    
+    private lazy var toggleSwitch: UISwitch = {
+        let toggle = UISwitch()
+        toggle.isOn = true
+        return toggle
+    }()
+    
+    private lazy var stackView: UIStackView = {
+        let stack = UIStackView(arrangedSubviews: [toggleLabel, toggleSwitch])
+        stack.axis = .horizontal
+        stack.spacing = 12
+        stack.alignment = .center
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        return stack
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        setupSelfView()
+        setupViews()
+    }
+    
+    private func setupSelfView() {
         view.backgroundColor = .red
+    }
+    
+    private func setupViews() {
+        view.addSubview(stackView)
+        
+        NSLayoutConstraint.activate([
+            stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
     }
 }
